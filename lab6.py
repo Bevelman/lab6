67c9f75e6bb0ce50d4009a3d0b1a2149adb457e8
Bevelman <91498634+Bevelman@users.noreply.github.com>
2021-11-20 04:55:33 +0000

    sfhsgsf

--- lab6.py
+++ lab6.py
@@ -9,27 +9,27 @@ for i in range(8):
   led.pattern[i]=0b00000000
   if i==4:
     led.pattern[i]=0b00010000
-
-while True:
-  for i in range(0,8):
-    if led.pattern[i]>0:
-      x=random.randint(0,3)
-      if x==0 and led.pattern[i]!=0b10000000: 
-        led.pattern[i]=led.pattern[i]<<1
-      elif x==1 and led.pattern[i]!=0b00000001: 
-        led.pattern[i]=led.pattern[i]>>1
-      elif x==2 and i!=0:
-        led.pattern[i-1]=led.pattern[i]
-        led.pattern[i]=0b00000000
-      elif x==3 and i!=7:
-        led.pattern[i+1]=led.pattern[i]
-        led.pattern[i]=0b00000000
-      break
-  time.sleep(0.1)
+try:
+  while True:
+    for i in range(0,8):
+      if led.pattern[i]>0:
+        x=random.randint(0,3)
+        if x==0 and led.pattern[i]!=0b10000000: 
+          led.pattern[i]=led.pattern[i]<<1
+        elif x==1 and led.pattern[i]!=0b00000001: 
+          led.pattern[i]=led.pattern[i]>>1
+        elif x==2 and i!=0:
+          led.pattern[i-1]=led.pattern[i]
+          led.pattern[i]=0b00000000
+        elif x==3 and i!=7:
+          led.pattern[i+1]=led.pattern[i]
+          led.pattern[i]=0b00000000
+        break
+    time.sleep(0.1)
         
 
 
-#except KeyboardInterrupt:   # catch everything, just in case
-#  led.p.terminate()      # terminate the process
-#  led.p.join(2)          # wait up to 2 sec for process termination
+except KeyboardInterrupt:   # catch everything, just in case
+  led.p.terminate()      # terminate the process
+  led.p.join(2)          # wait up to 2 sec for process termination
                          # before ending code
---
+++ led8x8s.py
@@ -0,0 +1,15 @@
+import time
+from shifter import Shifter
+
+class led8x8():
+  
+  def __init__(self, data, latch, clock):
+    self.shifter = Shifter(data, latch, clock)
+
+  def display(self, pat):
+    while True:
+      for i in range(len(pat)):
+        self.shifter.shiftByte(pat[i]) # load the row values
+        self.shifter.shiftByte(1 << (i)) # select the given row
+        self.shifter.latch()
+      time.sleep(0.001)
